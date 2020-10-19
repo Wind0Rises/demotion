738f4893fa90cec2982bbdb50129bdee02aeea90
liuweian <1931323324@qq.com>
2020-10-19 12:50:49 +0800

    first commit

---
+++ spring-hystrix/spring-hystrix-basis/src/main/java/com/liu/study/spring/cloud/hystrix/App.java
@@ -0,0 +1,13 @@
+package com.liu.study.spring.cloud.hystrix;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
